8977e96d12d82f85fa623ee3d9d1c496eb8a5f70
JasonEJohnson <johnson@take-e-way.de>
2024-03-18 12:09:58 +0100

    added Sidebar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Sidebar() {
+    return (
+        <div className="w-32 border-r h-screen shadow-md dark:text-white">
+            <div> Light / Dark </div>
+            <h1>Sidebar</h1>
+        </div>
+    );
+}
